008a879d8183cb9138a66a781e5b91aeb7270654
unknown <enzopinheiro6@gmail.com>
2022-09-02 14:23:32 -0300

    new update 02/09/2022

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,25 +8,45 @@ interface LayoutProps{
 }
 export default function Layout(props:LayoutProps){
     const [show, setShow] = useState ('1')
-    return (
-        <div className={`flex h-screen w-screen`}>             
-            <SideBar  
-                change1={() => setShow('1')}
-                change2={() => setShow('2')}
-            />
-            {
-                show === '1' ? 
-                    <div className="flex flex-col w-full justify-center">
-                        <Graphics></Graphics> 
-                    </div>
-                : 
-                    <div className="flex flex-col w-full justify-center">
-                        <div className="flex flex-col w-80% items-center">
-                            <Table></Table>
-                        </div>
-                    </div>
-            }
+    const [show2, setShow2] = useState('4')
 
+    function renderizarCadastrar(){
+        return (
+            <div className={`flex flex-col justify-center items-center`}>
+                <input type="text" className={`bg-gray-200 w-1/6 rounded-md`}/>
+                <input type="text" className={`bg-gray-200 mt-5 w-1/6`}/>
+                <input type="text" className={`bg-gray-200 mt-5 w-1/6`}/>
+                <button onClick={()=>setShow2('4')}>Adicionar</button>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+
+            <div className={`flex h-screen w-screen`}>             
+                <SideBar  
+                    change1={() => setShow('1')}
+                    change2={() => setShow('2')}
+                    
+                />
+                {
+                    show === '1' ? 
+                        <div className="flex flex-col w-full justify-center">
+                            <Graphics></Graphics> 
+                        </div>
+                    : 
+                        <div className="flex flex-col w-full justify-center">
+                            
+                            {show2 === '5' ? renderizarCadastrar(): <div className="flex flex-col w-80% items-center">
+                                <Table></Table>
+                            </div>}
+                                <button  className={`${show2 === '5' ? 'hidden' : ''}`}onClick={()=>setShow2('5')}>Cadastrar</button>
+                        </div>
+                }
+
+                
+            </div>
         </div>
     )
 }
