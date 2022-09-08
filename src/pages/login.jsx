af51084b4c5393f16f3c244a19d6f19a16aaaa12
unknown <enzopinheiro6@gmail.com>
2022-09-08 13:31:44 -0300

    080920221331

---
+++ src/pages/login.jsx
@@ -0,0 +1,7 @@
+export default function Login(){
+    return (
+        <div className={`flex flex-col w-screen h-screen justify-center items-center`}>
+            <button>Ola</button>
+        </div>
+    )
+}
